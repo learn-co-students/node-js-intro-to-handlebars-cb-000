37eb205c664e614a916afe7ed5ce853435453d04
tahbristol <tahbristol@gmail.com>
2018-02-27 23:39:54 -0500

    Done.

--- app/index.js
+++ app/index.js
@@ -111,6 +111,22 @@ app.post('/user', (req, res) => {
 	if (_.isEmpty(req.body))
     return res.sendStatus(400);
 
+
+			const { body } = req;
+
+		   if (!body.username || !body.password || !body['confirm-password']) {
+		     req.flash('error', 'All fields are required!');
+		     return res.redirect('/signup');
+		   }
+
+		   if (body.password !== body['confirm-password']) {
+		     req.flash('error', 'Password did not match confirmation!');
+		     return res.redirect('/signup');
+		   }
+
+		   delete body['confirm-password'];
+
+
   User
     .forge(req.body)
     .save()
